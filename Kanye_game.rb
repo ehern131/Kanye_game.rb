40883844fa97bafd5601df185ad1eada4face75a
George Rodriguez <georgesebastian2593@gmail.com>
2016-10-17 17:42:21 -0400

    rebl game

---
+++ Kanye_game.rb
@@ -0,0 +1,39 @@
+puts "Welcome to the Kanye West love game!"
+
+puts "This game will determine how much you love these certain things compared to how much Kanye West loves Kanye West."
+
+puts "You will use these three options to determine your love for Ice Cream"
+
+puts "love/really love/super love"
+option = gets.chomp
+case option
+when "love"
+ puts "You love Ice Cream but Kanye loves his dark chocolate self a lot more"
+ when "really love"
+ puts "You really love Ice Cream but Kanye loves his dark chocolate self a lot more"
+when "super love"
+puts "You super love Ice Cream but Kanye loves his dark chocolate self a lot more"
+end
+puts "How much do you love lamborghinis?"
+puts "love them/suppa love/in love"
+option = gets.chomp
+case option
+when "love them"
+puts "Skkkkrrttttttt, SWERVE! Kanye shows your love for lambos no
+mercy and loves himself way more!"
+when "suppa love"
+puts "Skkkkrrttttttt, SWERVE! You may suppa love lambos but Kanye loves himself way more!"
+when "in love"
+puts "Skkkkrrttttttt, SWERVE!You may be in love with lambos but Kanye West just bought 5 lambos for himself to show how much he's in love with himself!"
+end
+puts "How much do you love yourself?"
+puts  "hate myself/little bit/a lot/"
+option = gets.chomp
+case option
+when "hate myself"
+puts "Woah there,I think you might need some personal help.GAME OVER"
+when "little bit"
+puts "A little bit of love wont measure up to the amount of love Kanye west has for himself!. You lose."
+when "a lot"
+puts "HA!The amount of love Kanye West has for Kanye West is immeasurable! GAME OVER!"
+end
